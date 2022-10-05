9440a0eab6a5bf4259046d20a9a2816c8f7621b8
Marko Janjic <marko.m.janjic@gmail.com>
2022-10-05 22:39:03 +0200

    finished v1 of the layout

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let abTop = ab.getBoundingClientRect().top
 
 window.addEventListener('scroll',()=>{
     let value = window.scrollY
-    text.style.top = 50+ value * -0.5 +"%"
+    text.style.top = 50+ value * -0.3 +"%"
     bird1.style.top = value * -1.5 + "px"
     bird1.style.left = value * 2 + "px"
     bird2.style.top = value * -1.5 + "px"
